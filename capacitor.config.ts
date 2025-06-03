ecaf3a8b44897ffec0c62c1d3613cada688d1ac5
Marlon Harrison <92547717+marlon-ionic@users.noreply.github.com>
2025-06-03 12:27:57 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.demo.push',
+  appName: 'Push Link',
+  webDir: 'www'
+};
+
+export default config;
